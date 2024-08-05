08737727a934bdec2ef63bfe4fcda59efb4f82f7
Liudmila198 <belokopytova.ln@gmail.com>
2024-08-05 19:40:05 +0300

    2th/3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
+//var readlineSync = require('readline-sync');
+
+// Wait for user's response.
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hello, ' + userName + '!');
 
