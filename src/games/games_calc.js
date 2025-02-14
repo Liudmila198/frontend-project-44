b0d5a5611b6ca9a4d3042aa309f3963f8d54af19
Liudmila198 <belokopytova.ln@gmail.com>
2025-02-14 17:09:41 +0300

    six

--- src/games/games_calc.js
+++ src/games/games_calc.js
@@ -5,13 +5,13 @@ export const gamesCalc = () => {
   let userName = readlineSync.question("May I have your name? ");
   console.log("Hello, " + userName + "!");
 
-  const getRandomNumber = (min, max) => {
+  const randomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
   console.log('What is the result of the expression?');
 
-  const operators = ['+', '-', '*'];
+  const operators = ['+', '-', '*'];   
   
 const calculateOperations = (operators, num1, num2) => {
   let result;
@@ -30,6 +30,14 @@ const calculateOperations = (operators, num1, num2) => {
   }
   return `${result}`;
 };
+const getQuestionAndAnswer = () => {
+  const randomOperator = operators[randomNumber(0, 3)];
+  const num1 = randomNumber(0, 11);
+  const num2 = randomNumber(0, 11);
+  const anotherGameQuestion = `${num1} ${randomOperator} ${num2}`;
+  const expectedAnswer = calculateOperations(randomOperator, num1, num2);
+  return [anotherGameQuestion, expectedAnswer];
+};
   const calcGame = () => {
     for (let countAnswer = 0; countAnswer < 3; countAnswer += 1) {
           //let randomNumber = getRandomNumber(1, 100);
@@ -39,7 +47,7 @@ const calculateOperations = (operators, num1, num2) => {
           if (answer === getQuestionAndAnswer()) {
             console.log("Correct!");
           } else {
-            console.log(`${answer} is wrong answer ;(. Correct answer was ${getQuestionAndAnswer()}`               //${correctAnswer}`
+            console.log(`${answer} is wrong answer ;(. Correct answer was ${calculateOperations(operators, num1, num2)}`               //${correctAnswer}`
             )
             break;};
         }
