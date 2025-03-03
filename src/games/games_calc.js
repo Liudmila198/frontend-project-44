0a86319d5ad47b8cee33cc101d28cdbd184f9081
Liudmila198 <belokopytova.ln@gmail.com>
2025-03-03 13:21:15 +0300

    calc-commit

--- src/games/games_calc.js
+++ src/games/games_calc.js
@@ -13,46 +13,46 @@ export const gamesCalc = () => {
 
   const operators = ['+', '-', '*'];   
   
-const calculateOperations = (operators, num1, num2) => {
-  let result;
-  switch (operators) {
-    case '+':
-      result = num1 + num2;
-      break;
-    case '-':
-      result = num1 - num2;
-      break;
-    case '*':
-      result = num1 * num2;
-      break;
-    default:
-      return undefined;
-  }
-  return `${result}`;
-};
-const getQuestionAndAnswer = () => {
-  const randomOperator = operators[randomNumber(0, 3)];
-  const num1 = randomNumber(0, 11);
-  const num2 = randomNumber(0, 11);
-  console.log(111111, num1, num2)
-  const anotherGameQuestion = `${num1} ${randomOperator} ${num2}`;
-  const expectedAnswer = calculateOperations(randomOperator, num1, num2);
-  return [anotherGameQuestion, expectedAnswer];
-};
+  const calculateOperations = (operator, num1, num2) => {
+  //let result;
+    //console.log(444, operator, num1, num2);
+    switch (operator) {
+      case '+':
+        return num1 + num2;
+    //console.log(333, result) 
+      case '-':
+        return num1 - num2;
+        //break;
+      case '*':
+        return num1 * num2;
+        //break;
+      default:
+        return NaN;
+    }    
+  };
+  const getQuestionAndAnswer = () => {
+    const randomOperator = operators[randomNumber(0, 3)];
+    const num1 = randomNumber(6, 11);
+    const num2 = randomNumber(0, 6);
+    const anotherGameQuestion = `${num1} ${randomOperator} ${num2}`;
+    const expectedAnswer = calculateOperations(randomOperator, num1, num2);
+console.log(55555, expectedAnswer);
+    return [anotherGameQuestion, expectedAnswer];
+    
+  };
   const calcGame = () => {
     for (let countAnswer = 0; countAnswer < 3; countAnswer += 1) {
-          //let randomNumber = getRandomNumber(1, 100);
-          console.log(`"Question:" ${}`);
-          const answer = readlineSync.question("Your answer: ");
-          //let correctAnswer = calculateOperations(randomOperator, num1, num2);
-          if (answer === getQuestionAndAnswer()) {
-            console.log("Correct!");
-          } else {
-            console.log(`${answer} is wrong answer ;(. Correct answer was ${calculateOperations(operators, num1, num2)}`               //${correctAnswer}`
-            )
-            break;};
-        }
-        console.log("Congratulation");
+          const questionA = getQuestionAndAnswer()[0];
+          const rightAnswer = getQuestionAndAnswer()[1];
+          console.log(`"Question:" ${questionA}`);
+          const myAnswer = readlineSync.question("Your answer: " );
+              if (myAnswer === rightAnswer) {
+                console.log("Correct!");
+              } else {
+                console.log(`${myAnswer} is wrong answer ; Correct answer was ${rightAnswer}`)
+                break;};
+            }
+            console.log("Congratulation");
       };
   calcGame();
 };
