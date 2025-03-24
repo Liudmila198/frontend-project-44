b1cf2c256ffa213784eacf65403d85f27a007294
Liudmila198 <Belokopytova.ln@gmail.com>
2025-03-24 15:58:19 +0300

    26

--- src/games/games_calc.js
+++ src/games/games_calc.js
@@ -2,35 +2,38 @@ import readlineSync from "readline-sync";
 import { userName, getRandomNumber } from './index.js';
 export const gamesCalc = () => {
   console.log('What is the result of the expression?');
-  
+
   const operators = ['+', '-', '*'];   
 
   const calculateOperations = (operator, num1, num2) => {
+    let result;
     switch (operator) {
       case '+':
-        return num1 + num2;
+        result = num1 + num2;
+        break;
       case '-':
-        return num1 - num2;
+        result = num1 - num2;
+        break;
       case '*':
-        return num1 * num2;
+        result = num1 * num2;
+        break;
       default:
-        return undefined
-      // default:
-      //   return NaN;
-    }    
+        return undefined;
+    }
+    return `${result}`;
   };
   const getQuestionAndAnswer = () => {
     const randomOperator = operators[getRandomNumber(0, 3)];
     const num1 = getRandomNumber(6, 11);
     const num2 = getRandomNumber(0, 6);
-    const anotherGameQuestion = `${num1} ${randomOperator} ${num2}`;
+    const question = `${num1} ${randomOperator} ${num2}`;
     const expectedAnswer = calculateOperations(randomOperator, num1, num2);
-    return [anotherGameQuestion, expectedAnswer];
+    return [question, expectedAnswer];
   };
   const calcGame = () => {
     for (let countAnswer = 0; countAnswer < 3; countAnswer += 1) {
-         const [anotherGameQuestion, expectedAnswer] = getQuestionAndAnswer();
-          console.log(`"Question:" ${anotherGameQuestion}`);
+         const [question, expectedAnswer] = getQuestionAndAnswer();
+          console.log(`"Question:" ${question}`);
           const myAnswer = readlineSync.question("Your answer: " );
               if (myAnswer == expectedAnswer) {
                 console.log("Correct!");
