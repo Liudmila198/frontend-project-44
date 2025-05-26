5b0cc55f64f6df0fe67ad4a3a9db7c525b74d6fe
liudmila198 <belokopytova.ln@gmail.com>
2025-05-26 11:09:40 +0300

    54

--- src/games/games_even.js
+++ src/games/games_even.js
@@ -1,43 +1,35 @@
-import readlineSync from "readline-sync";
-
+import readlineSync from 'readline-sync'
 export const gamesEven = () => {
-  console.log("Welcome to the Brain Games!");
+  console.log('Welcome to the Brain Games!')
 
-  let userName = readlineSync.question("May I have your name? ");
-  console.log("Hello, " + userName + "!");
+  let userName = readlineSync.question('May I have your name? ')
+  console.log('Hello, ' + userName + '!')
 
   const getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
+  }
 
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log('Answer "yes" if the number is even, otherwise answer "no".')
 
-  const isEven = (number) => number % 2 === 0;
-
-  const generateRound = () => {
-    const number = getRandomNumber(1, 100);
-    const question = number.toString();
-    const correctAnswer = isEven(number) ? "yes" : "no";
-    return [question, correctAnswer];
-  };
-
+  const rightAnswer = (randomNumber) => (randomNumber % 2 === 0 ? 'yes' : 'no')
   const evenGame = () => {
     for (let countAnswer = 0; countAnswer < 3; countAnswer += 1) {
-      const [question, correctAnswer] = generateRound();
-      console.log(`Question: ${question}`);
-      const answer = readlineSync.question("Your answer: ");
-      //let correctAnswer = rightAnswer(question);
+      let randomNumber = getRandomNumber(1, 100);
+      console.log('Question:' + ' ' + randomNumber)
+      const answer = readlineSync.question('Your answer: ')
+      let correctAnswer = rightAnswer(randomNumber)
       if (answer === correctAnswer) {
-        console.log("Correct!");
+        console.log('Correct!')
       } else {
         console.log(
-          `"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`
-        );
-        console.log(`Let's try again, ${userName}!`);
-        return;
-      }
+          `${answer} is wrong answer ;(. Correct answer was ${correctAnswer}`
+        
+        )
+        break;}
     }
-    console.log(`Congratulations, ${userName}!`);
+    console.log('Congratulation')
   };
-  evenGame();
+  evenGame()
 };
+
+
