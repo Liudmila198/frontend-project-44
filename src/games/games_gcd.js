ec918f9f83051cd6d2fbb24cc71d97a9e61d3aa6
liudmila198 <belokopytova.ln@gmail.com>
2025-06-15 19:12:00 +0300

    87

--- src/games/games_gcd.js
+++ src/games/games_gcd.js
@@ -21,5 +21,5 @@ export const gamesGcd = () => {
     const expectedAnswer = gcd(num1, num2).toString()
     return [anotherGameQuestion, expectedAnswer]
   }
-  calcGame = createGame (gameDescription, getQuestionAndAnswer)
+  createGame (gameDescription, getQuestionAndAnswer)
 }
